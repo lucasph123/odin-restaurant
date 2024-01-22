83032e10b3c6e729082ca9a183573eea2c4f37f1
Lucas <41449194+lucasph123@users.noreply.github.com>
2024-01-22 17:50:38 -0300

    added contact tab

--- src/contact.js
+++ src/contact.js
@@ -8,11 +8,47 @@ function createContact()
     const main = document.querySelector('#container');
     contact.classList.add('menu')
     main.removeChild(main.lastChild);
-    contact.textContent = 'contact';
     main.appendChild(contact);
-    
+
+    displayContact(contact);
     return contact;
 
+}
+
+function displayContact(contact)
+{
+    const contactContainer = document.createElement('div');
+    contactContainer.classList.add('contact-container');
+    const contactTitle = document.createElement('div');
+    contactTitle.classList.add('title');
+    contactTitle.textContent = 'Contacts';
+    contact.appendChild(contactTitle);
+    contact.appendChild(contactContainer);
+
+    const contactDesc = document.createElement('div');
+    contactDesc.classList.add('contact-desc');
+    const contactsObj = contacts();
+    for (const key in contactsObj.contacts) {
+        const contactList = document.createElement('div');
+        contactList.textContent =key +': '+ contactsObj.contacts[key];
+        contactDesc.appendChild(contactList);
+    }
+    contactContainer.appendChild(contactDesc);
+
+}
+
+
+
+function contacts()
+{
+    const contacts = {
+        emailOne: 'EmailOne@email.com',
+        emailTwo: 'EmailTwo@email.com',
+        telOne: '1234-1234',
+        telTwo: '6789-6789',
+        adress: '43 Fulton Rd.Lakeville, MN 55044',
+    }
+    return {contacts};
 }
 
 export default createContact;
